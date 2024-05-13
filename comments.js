eeebc1d53cb6c9ac3fec4431f3459a3ec8fd7cd6
Martin Marburger <marburgermartin143@googlemail.com>
2024-05-13 12:54:13 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,27 +1,40 @@
-const http = require('http');
+// Create web server
+const express = require('express');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
 const fs = require('fs');
-const path = require('path');
 
-const PORT = 3000;
+app.use(bodyParser.json());
 
-const server = http.createServer((req, res) => {
-    // Handling the home route
-    if (req.url === '/' || req.url === '/index.html') {
-        fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
-            if (err) {
-                res.writeHead(500, {'Content-Type': 'text/plain'});
-                res.end('Internal Server Error');
-            } else {
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.end(data);
-            }
-        });
-    } else {
-        res.writeHead(404, {'Content-Type': 'text/plain'});
-        res.end('404 Not Found');
-    }
+app.get('/comments', (req, res) => {
+    fs.readFile('./comments.json', 'utf-8', (err, data) => {
+        if (err) {
+            res.status(500).send('Error reading comments.json');
+        } else {
+            res.send(data);
+        }
+    });
 });
 
-server.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.post('/comments', (req, res) => {
+    fs.readFile('./comments.json', 'utf-8', (err, data) => {
+        if (err) {
+            res.status(500).send('Error reading comments.json');
+        } else {
+            const comments = JSON.parse(data);
+            comments.push(req.body);
+            fs.writeFile('./comments.json', JSON.stringify(comments), (err) => {
+                if (err) {
+                    res.status(500).send('Error writing comments.json');
+                } else {
+                    res.send(comments);
+                }
+            });
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
